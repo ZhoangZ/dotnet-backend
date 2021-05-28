d49005ae9c31399185824be91e88a9ad8721500a
ongdinh6 <ongdinh6@gmail.com>
2021-05-28 21:15:53 +0700

    add update-user-info

--- backend-dotnetcore/BackendDotnetCore/Controllers/UsersController.cs
+++ backend-dotnetcore/BackendDotnetCore/Controllers/UsersController.cs
@@ -3,6 +3,7 @@ using BackendDotnetCore.Services;
 using Microsoft.AspNetCore.Http;
 using System.Security.Claims;
 using BackendDotnetCore.Entities;
+using BackendDotnetCore.Forms;
 using Microsoft.AspNetCore.Mvc;
 using System;
 
@@ -50,6 +51,35 @@ namespace WebApi.Controllers
             }
 
         }
+      
+        [HttpPut("edit/{id}")]
+        public IActionResult EditUserInfo(int id, UserEntity info)
+        {
+            Console.WriteLine("Edit user: "+id+", "+info.Fullname);
+            UserEntity ueUpdate = _userService.getUserById(id);
+            if (null != ueUpdate)
+            {
+                info.Id = id;
+                info.Password = ueUpdate.Password;
+                if (!ueUpdate.Email.Equals(info.Email))
+                    if (_userService.checkEmail(info.Email) == true) return BadRequest(new { message = "Email cập nhật đã tồn tại trong hệ thống! Thử lại với một mail khác." });
+                ueUpdate = info;
+                bool updated = _userService.save(ueUpdate);
+                if (updated == true)
+                {
+                    return Ok(_userService.createUserJWT(ueUpdate));
+                }
+                else
+                {
+                    return BadRequest(new { message = " Hệ thống đang gặp sự cố!" });
+                }
+            }
+            else
+            {
+                return  BadRequest(new { message= " Có lỗi xảy ra với user!"});
+            }
+        }
+
        
     }
 }
