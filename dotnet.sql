5d9cb68deb205a7998951440bba50eb85e0b692f
ZHoangZ <hearterzouest99.999@gmail.com>
2021-05-30 21:24:14 +0700

    merge mysql

--- dotnet.sql
+++ dotnet.sql
@@ -11,7 +11,7 @@
  Target Server Version : 100417
  File Encoding         : 65001
 
- Date: 30/05/2021 21:20:59
+ Date: 30/05/2021 21:23:50
 */
 
 SET NAMES utf8mb4;
@@ -132,14 +132,29 @@ CREATE TABLE `comment`  (
   `id` int(10) NOT NULL AUTO_INCREMENT,
   `user_id` int(10) NULL DEFAULT NULL,
   `product_id` int(10) NULL DEFAULT NULL,
+  `rate` double(1, 0) NULL DEFAULT NULL,
   `content` longtext CHARACTER SET utf8 COLLATE utf8_unicode_ci NULL,
+  `created_date` date NULL DEFAULT NULL,
   `active` int(255) NULL DEFAULT 1,
   PRIMARY KEY (`id`) USING BTREE,
   INDEX `user_id`(`user_id`) USING BTREE,
   INDEX `product_id`(`product_id`) USING BTREE,
-  CONSTRAINT `comment_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  CONSTRAINT `comment_ibfk_2` FOREIGN KEY (`product_id`) REFERENCES `product` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_unicode_ci ROW_FORMAT = Dynamic;
+  CONSTRAINT `comment_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `comment_ibfk_2` FOREIGN KEY (`product_id`) REFERENCES `product_2` (`ID`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE = InnoDB AUTO_INCREMENT = 13 CHARACTER SET = utf8 COLLATE = utf8_unicode_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of comment
+-- ----------------------------
+INSERT INTO `comment` VALUES (4, 17, 10, NULL, 'San pham nay rat tuyet', NULL, 1);
+INSERT INTO `comment` VALUES (5, 17, 11, 0, 'San pham nay rat tuyet', '0001-01-01', 1);
+INSERT INTO `comment` VALUES (6, 19, 11, 4, 'San pham nay rat tuyet boi 19', '2021-05-30', 1);
+INSERT INTO `comment` VALUES (7, 19, 11, 4, 'San pham nay rat tuyet boi 19', '2021-05-30', 1);
+INSERT INTO `comment` VALUES (8, 18, 11, 4, 'San pham nay rat tuyet boi 19', '2021-05-30', 1);
+INSERT INTO `comment` VALUES (9, 16, 11, 5, 'San pham nay rat tuyet boi 16', '2021-05-30', 1);
+INSERT INTO `comment` VALUES (10, 15, 11, 4, 'San pham nay rat tuyet boi 15', '2021-05-30', 1);
+INSERT INTO `comment` VALUES (11, 14, 11, 4, 'San pham nay rat tuyet boi 14', '2021-05-30', 1);
+INSERT INTO `comment` VALUES (12, 14, 11, 4, 'San pham nay rat tuyet boi 14', '2021-05-30', 1);
 
 -- ----------------------------
 -- Table structure for detail_product
